51cd7aea18c9708f26d267ee9ecd974b26b6715a
DKVilinskis <125027620+DKVilinskis@users.noreply.github.com>
2023-03-05 22:05:22 +0000

    attempt1

--- tree_height.py
+++ tree_height.py
@@ -2,30 +2,43 @@
 
 import sys
 import threading
-import numpy
+import numpy as np
 
 
 def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+    seen = np.zeros([n],dtype=np.int32)
+    maxH = np.zeros([n],dtype=np.int32)
+    
+    for i in range(n):
+        if seen[i] == 0:
+            times = 0
+            current = i
+            while current != -1:
+                times = times + 1
+                maxH = times
+                current = parents[current]
+                seen[current] = 1
+    maxH = max(maxH)
+    return maxH
 
 
 def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
+    inputType = input()
+    if "I" in inputType:
+        numbers = int(input())
+        parents = list(map(int, input().split()))
+        computed = compute_height(numbers,parents)
+        print(computed)
+    elif "F" in inputType:
+        fileName = input()
+        if "a" not in fileName:
+            file = "test/" + fileName
+            with open(file,"r") as fi:
+                numbers = int(fi.readline())
+                parents = list(map(int, fi.readline().strip().split()))
+                compuded = compute_height(numbers, parents)
+                print(compuded)
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
